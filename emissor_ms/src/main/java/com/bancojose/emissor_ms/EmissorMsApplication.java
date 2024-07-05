78a911a969e02603d97fa37b7023610f5f66cf8a
José Roberto Barbosa <joserobertobarbosabezerra@gmail.com>
2024-07-04 22:16:07 -0300

    emissor de cartoes

---
+++ emissor_ms/src/main/java/com/bancojose/emissor_ms/EmissorMsApplication.java
@@ -0,0 +1,13 @@
+package com.bancojose.emissor_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmissorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmissorMsApplication.class, args);
+	}
+
+}
